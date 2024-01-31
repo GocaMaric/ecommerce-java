b8a38fdf26e9c90eb57da511c165802a722e1740
gocamaric <gocamaaric@hotmail.com>
2024-01-31 11:23:52 +0100

    1 commit.

--- src/main/java/com/goca/ecommercejava/EcommerceJavaApplication.java
+++ src/main/java/com/goca/ecommercejava/EcommerceJavaApplication.java
@@ -3,11 +3,29 @@ package com.goca.ecommercejava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class EcommerceJavaApplication {
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 
-	public static void main(String[] args) {
-		SpringApplication.run(EcommerceJavaApplication.class, args);
-	}
+@SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+        title="Ecommerce Project Api With Springboot",
+        description="Ecommerce backend project with springboot and spring security",
+        version="v1",
+        contact=@Contact(
+                name="Code With Goca",
+                email="goranmaaric@gmail.com"
+        ),
+        license=@License(
+                name="Kodiraj sa Gocom"
+        )
+))
+public class ECommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceApplication.class, args);
+    }
 
 }
