a9a0cc4756696ed010ff0edb862a4d4858ab4aad
gocamaric <gocamaaric@hotmail.com>
2024-02-07 12:17:45 +0100

    10 commit. Error D and Cart

---
+++ src/main/java/com/goca/ecommercejava/exception/CartItemException.java
@@ -0,0 +1,10 @@
+package com.goca.ecommercejava.exception;
+
+public class CartItemException extends Exception {
+
+    public CartItemException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+}
