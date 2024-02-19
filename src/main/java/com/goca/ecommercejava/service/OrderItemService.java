25ebaafb81ae4fac6efeb4895d73a4c712402154
gocamaric <gocamaaric@hotmail.com>
2024-02-19 12:12:42 +0100

    6. Cart Item Service

---
+++ src/main/java/com/goca/ecommercejava/service/OrderItemService.java
@@ -0,0 +1,9 @@
+package com.goca.ecommercejava.service;
+
+import com.goca.ecommercejava.modal.OrderItem;
+
+public interface OrderItemService {
+
+    public OrderItem createOrderItem(OrderItem orderItem);
+
+}
